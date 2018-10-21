0b5cd4f289a0139d3824a2f7407cde6ee8367fa0
mibikin <mibikin@gmail.com>
2018-10-21 01:00:24 -0600

    Updated Projectile

--- core/src/com/westerndentist/game/Projectile.java
+++ core/src/com/westerndentist/game/Projectile.java
@@ -1,11 +1,27 @@
 package com.westerndentist.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Projectile extends Actor{
 
     private Texture texture = new Texture("");
+    private float speed = 0;
 
-    
+    Projectile(Texture texture, float initialSpeed, float x, float y) {
+        this.texture = texture;
+        speed = initialSpeed;
+        setPosition(x, y);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(texture, getX(), getY());
+    }
+
+    @Override
+    public void act(float delta) {
+        moveBy(getX(), getY() + speed * delta);
+    }
 }
