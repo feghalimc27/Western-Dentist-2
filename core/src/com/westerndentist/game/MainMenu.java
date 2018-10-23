bcd8d8c70029f8a762810a38a165b29549c4119d
uriahmoeller <uriah.moeller@gmail.com>
2018-10-23 15:25:31 -0600

    Code Refactor, changeStage Method
    SplashScreen now changes to MainMenu at end of splash. SplashScreen doesn't use scene2d.ui, just scene2d. Added helper method for changing stage. Use SplashScreen and MainMenu constructors as reference for new Stages (final WesternDentist, final FitViewport, then call super(viewport) before doing anything else).
    
    As a note, all stage UIs should use stage2d.ui library. I'll be doing the MainMenu class this way, you can use it as reference when I'm done.

--- core/src/com/westerndentist/game/MainMenu.java
+++ core/src/com/westerndentist/game/MainMenu.java
@@ -1,41 +1,18 @@
 package com.westerndentist.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenu extends Stage {
-    MainMenu(Viewport viewport) {
-        super(viewport);
-        splashScreen();
-    }
+    private WesternDentist game;
+    private Table table;
 
-    public void splashScreen() {
-        Image splash = new Image(new Texture("images/JEGA.jpg"));
-        SequenceAction sequence = Actions.sequence();
-        sequence.addAction(Actions.alpha(0));
-        sequence.addAction(Actions.delay(1));
-        sequence.addAction(Actions.fadeIn(1));
-        sequence.addAction(Actions.run(new Runnable() {
-            @Override
-            public void run() {
-                Sound intro = Gdx.audio.newSound(Gdx.files.internal("sounds/JEGA.mp3"));
-                intro.play(1.0f);
-            }
-        }));
-        sequence.addAction(Actions.delay(2.5f));
-        sequence.addAction(Actions.fadeOut(1));
-        sequence.addAction(Actions.delay(1));
-        splash.addAction(sequence);
-        addActor(splash);
+    MainMenu(final WesternDentist game, final FitViewport viewport) {
+        super(viewport);
+        this.game = game;
+        table = new Table();
+        table.setFillParent(true);
+        table.center();
     }
 }
--- core/src/com/westerndentist/game/WesternDentist.java
+++ core/src/com/westerndentist/game/WesternDentist.java
@@ -1,27 +1,22 @@
 package com.westerndentist.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class WesternDentist extends Game {
-    public BitmapFont font;
-    public Stage currentStage;
+    private BitmapFont font;
+    private Stage currentStage;
+    private FitViewport viewport;
 
 	@Override
 	public void create () {
 	    font = new BitmapFont();
-        currentStage = new ExampleLevel(new FitViewport(800, 600));
+	    viewport = new FitViewport(800, 600);
+        currentStage = new SplashScreen(this, viewport);
         Gdx.input.setInputProcessor(currentStage);
 	}
 
@@ -37,5 +32,12 @@ public class WesternDentist extends Game {
 	@Override
     public void resize (int width, int height) {
         currentStage.getViewport().update(width, height, true);
+    }
+
+    public void changeStage(Stage newStage) {
+	    currentStage.clear();
+	    currentStage.dispose();
+        currentStage = newStage;
+        Gdx.input.setInputProcessor(currentStage);
     }
 }
