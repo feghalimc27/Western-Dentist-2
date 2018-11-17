c00f4f45ef8c4ed9c7159ddf83bb18fe9d6dab48
mibikin <mibikin@gmail.com>
2018-11-17 16:12:47 -0700

    Added new enemies, fixed spray enemy, added boss to level 4

---
+++ core/src/com/westerndentist/game/BossLevel4.java
@@ -0,0 +1,159 @@
+package com.westerndentist.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class BossLevel4 extends Boss {
+
+    private float phase1damage = 500, phase2damage = 1000,
+                  phase3damage = 2000, phase4damage = 4000;
+
+    private Texture texture = new Texture("Images/l4BossSprite.png");
+
+    private int phase = 1;
+
+    private float proj1xFactor = 1000, proj1yFactor = 0;
+
+    private float rateCounter1 = 0;
+    private float fireRate1 = 10;
+
+    private float damage = 0;
+
+    private boolean spawned = false;
+
+    BossLevel4() {
+        super();
+
+        bounds = new Rectangle(getX(), getY(), texture.getWidth(), texture.getHeight());
+    }
+
+    @Override
+    public void act(float delta) {
+        if (!spawned) {
+            spawned = true;
+            // Play sound, do stuff
+            Gdx.app.log("Boss 4", "Spawned");
+        }
+        super.act(delta);
+        takeDamageFromProjectile();
+        changePhase();
+        phase(delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(texture, getX(), getY());
+    }
+
+    private void changePhase() {
+        if (damage > phase1damage) {
+            phase = 2;
+        }
+        else if (damage > phase2damage) {
+            phase = 3;
+        }
+        else if (damage > phase3damage) {
+            phase = 4;
+        }
+        else if (damage > phase4damage) {
+            // end level
+            phase = 5;
+        }
+    }
+
+    private void takeDamageFromProjectile() {
+        try {
+            for (Actor actor : getStage().getActors()) {
+                if (Enemy.class.isInstance(actor)) {
+                    continue;
+                }
+
+                if (Projectile.class.isInstance(actor)) {
+                    if (bounds.overlaps(((Projectile) actor).getBounds())) {
+                        if (actor.getName() != "Enemy") {
+                            damage += ((Projectile) actor).getDamage();
+                            ((Projectile) actor).destroy();
+                        }
+                    }
+                }
+            }
+        } catch (NullPointerException e) {
+            Gdx.app.log("Enemy: ", "Something broke but I'm just gonna ignore it lol");
+        }
+    }
+
+    private void phase(float delta) {
+        switch (phase) {
+            case 1:
+                phase1(delta);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void phase1(float delta) {
+        float spinFactor = 1000;
+
+        if (proj1xFactor <= 1005 && proj1xFactor > -5 && proj1yFactor >= -5) {
+            proj1xFactor -= spinFactor * delta;
+            proj1yFactor += spinFactor * delta;
+        }
+        else if (proj1xFactor <= -5 && proj1xFactor > -1005 && proj1yFactor > -5) {
+            proj1xFactor -= spinFactor * delta;
+            proj1yFactor -= spinFactor * delta;
+        }
+        else if (proj1xFactor > -1005 && proj1xFactor < -5 && proj1yFactor >= -1005) {
+            proj1xFactor += spinFactor * delta;
+            proj1yFactor -= spinFactor * delta;
+        }
+        else if (proj1xFactor >= -5 && proj1xFactor < 1005 && proj1yFactor > -1005) {
+            proj1xFactor += spinFactor * delta;
+            proj1yFactor += spinFactor * delta;
+        }
+
+        if (rateCounter1 == 0) {
+            try {
+                getStage().addActor(new NonVerticalProjectile(new Texture("Images/WesternDentist_BossBurst.png"), 100, getX() + texture.getWidth() / 2, getY() + texture.getHeight() / 2, "Enemy", proj1xFactor / 1000, proj1yFactor / 1000, false, false));
+            } catch (NullPointerException e) {
+
+            }
+
+            if (proj1xFactor <= 1100 && proj1xFactor > 0 && proj1yFactor >= -100) {
+                proj1xFactor -= spinFactor * delta;
+                proj1yFactor += spinFactor * delta;
+            }
+            else if (proj1xFactor <= 0 && proj1xFactor > -1000 && proj1yFactor > 0) {
+                proj1xFactor -= spinFactor * delta;
+                proj1yFactor -= spinFactor * delta;
+            }
+            else if (proj1xFactor > -1100 && proj1xFactor < 0 && proj1yFactor > -1100) {
+                proj1xFactor += spinFactor * delta;
+                proj1yFactor -= spinFactor * delta;
+            }
+            else if (proj1xFactor >= 0 && proj1xFactor < 1000 && proj1yFactor > -1100) {
+                proj1xFactor += spinFactor * delta;
+                proj1yFactor += spinFactor * delta;
+            }
+
+            rateCounter1 = fireRate1;
+        }
+        else {
+            rateCounter1 -= 1000 * delta;
+        }
+
+        if (rateCounter1 <= 0) {
+            rateCounter1 = 0;
+        }
+
+    }
+}
--- core/src/com/westerndentist/game/Level4.java
+++ core/src/com/westerndentist/game/Level4.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -26,9 +27,9 @@ public class Level4 extends Stage {
         addActor(background);
         addActor(background2);
 
-        SprayEnemy testActor = new SprayEnemy(new Texture("Images/tempRedCircle.png"), 100, 300);
-        testActor.setPosition(300, 300);
-        addActor(testActor);
+        SprayEnemy testActor = new SprayEnemy(new Texture("Images/sprayEnemySprite.png"), 100, 300);
+        testActor.setPosition(400, 300);
+        testActor.addAction(Actions.moveTo(400, 550, 5));
 
         addActor(new Player(300, 100));
 
@@ -36,15 +37,41 @@ public class Level4 extends Stage {
 
         sequencer.addPhase(300);
         sequencer.addPhase(400);
-        sequencer.addPhase(1000);
+        sequencer.addPhase(100000);
 
-        sequencer.addActorToPhase(0, new BasicEnemy(new Texture("Images/tempRedCircle.png"), 300, 10, 30, new Vector2(0, 0)));
-        sequencer.addPhaseSpawnFrequency(0, 10);
-        sequencer.addPhaseSpawnPosition(0, new Vector2(300, 700));
+        sequencer.addActorToPhase(0, new DiagonalEnemy(new Texture("Images/basicEnemySprite.png"), 10, 100, -200));
+        sequencer.addPhaseSpawnFrequency(0, 30);
+        sequencer.addPhaseSpawnPosition(0, new Vector2(600, 700));
 
-        sequencer.addActorToPhase(0, new BasicEnemy(new Texture("Images/tempRedCircle.png"), 300, 10, 30, new Vector2(0, 0)));
-        sequencer.addPhaseSpawnFrequency(0, 40);
-        sequencer.addPhaseSpawnPosition(0, new Vector2(330, 700));
+        sequencer.addActorToPhase(0, new DiagonalEnemy(new Texture("Images/basicEnemySprite.png"), 10, 100, 200));
+        sequencer.addPhaseSpawnFrequency(0, 30);
+        sequencer.addPhaseSpawnPosition(0, new Vector2(0, 700));
+
+        sequencer.addActorToPhase(0, new DiagonalEnemy(new Texture("Images/basicEnemySprite.png"), 10, 100, -200));
+        sequencer.addPhaseSpawnFrequency(0, 60);
+        sequencer.addPhaseSpawnPosition(0, new Vector2(600, 700));
+
+        sequencer.addActorToPhase(0, new DiagonalEnemy(new Texture("Images/basicEnemySprite.png"), 10, 100, 200));
+        sequencer.addPhaseSpawnFrequency(0, 60);
+        sequencer.addPhaseSpawnPosition(0, new Vector2(0, 700));
+
+        sequencer.addActorToPhase(1, testActor);
+        sequencer.addPhaseSpawnFrequency(1, 30);
+        sequencer.addPhaseSpawnPosition(1, new Vector2(0, 1000));
+
+        SprayEnemy nextActor = new SprayEnemy(new Texture("Images/sprayEnemySprite.png"), 100, 300);
+        nextActor.addAction(Actions.moveTo(100, 550, 5));
+
+        sequencer.addActorToPhase(1, nextActor);
+        sequencer.addPhaseSpawnFrequency(1, 33);
+        sequencer.addPhaseSpawnPosition(1, new Vector2(0, 1000));
+
+        BossLevel4 boss = new BossLevel4();
+        boss.addAction(Actions.moveTo(300, 400, 6));
+
+        sequencer.addActorToPhase(2, boss);
+        sequencer.addPhaseSpawnFrequency(2, 100);
+        sequencer.addPhaseSpawnPosition(2, new Vector2(300, 1000));
 
         this.game = game;
     }
@@ -89,15 +116,15 @@ public class Level4 extends Stage {
     }
 
     private void backgroundScrolling(float delta) {
-        background.setY(background.getY() + 30 * delta);
-        background2.setY(background2.getY() + 30 * delta);
+        background.setY(background.getY() - 30 * delta);
+        background2.setY(background2.getY() - 30 * delta);
 
-        if (background.getY() >= 5000) {
-            background.setY(-5000);
+        if (background.getY() <= -5000) {
+            background.setY(5000);
         }
 
-        if (background2.getY() >= 5000) {
-            background2.setY(-5000);
+        if (background2.getY() <= -5000) {
+            background2.setY(5000);
         }
     }
 }
