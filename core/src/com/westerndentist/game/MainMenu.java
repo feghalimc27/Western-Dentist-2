5ad9ce72d0a0737535d6c244cc4e8ed23bb2173e
mibikin <mibikin@gmail.com>
2018-12-03 15:24:16 -0700

    Update MainMenu.java

--- core/src/com/westerndentist/game/MainMenu.java
+++ core/src/com/westerndentist/game/MainMenu.java
@@ -36,7 +36,7 @@ public class MainMenu extends Stage {
         startButton.addListener(new InputListener(){
             @Override
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                game.changeStage(new Level4(game));
+                game.changeStage(new Level1(game));
                 return true;
             }
             @Override
