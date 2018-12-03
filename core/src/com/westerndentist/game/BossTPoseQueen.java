5bc1902dff556e5aedd31bf5db13ea878439f7b9
Adrian <adrian.dagher@gmail.com>
2018-12-02 20:46:30 -0700

    new

--- core/src/com/westerndentist/game/BossTPoseQueen.java
+++ core/src/com/westerndentist/game/BossTPoseQueen.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 public class BossTPoseQueen extends Boss {
 
     private float phase1damage = 20000, phase2damage = 40000,
-            phase3damage = 700000, phase4damage = 120000;
+            phase3damage = 70000, phase4damage = 120000;
 
     private Texture texture = new Texture("Images/L2_Boss_T-Pose_Queen.png");
 
